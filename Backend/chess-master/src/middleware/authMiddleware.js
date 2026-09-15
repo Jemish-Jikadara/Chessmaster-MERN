e18258458da755a4f66bf002f90a6e13b3187264
unknown <jemish.jikadara2007@gmail.com>
2026-09-15 16:29:48 +0530

    middelware mising funtion add

--- Backend/chess-master/src/middleware/authMiddleware.js
+++ Backend/chess-master/src/middleware/authMiddleware.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-
 function isAuthenticated(req, res, next) {
   if (req.session && req.session.user) {
     return next();
@@ -27,6 +26,23 @@ function isAuthenticated(req, res, next) {
 
   return res.status(401).json({ success: false, message: "Please login first." });
 }
+
+function isGuest(req, res, next) {
+  if (req.session && req.session.user) {
+    return res.status(400).json({ success: false, message: "Already logged in." });
+  }
+
+  return next();
+}
+
+function isAdmin(req, res, next) {
+  if (req.session && req.session.user && req.session.user.role === "admin") {
+    return next();
+  }
+
+  return res.status(403).json({ success: false, message: "You are not allowed to access this resource." });
+}
+
 module.exports = {
   isAuthenticated,
   isGuest,
